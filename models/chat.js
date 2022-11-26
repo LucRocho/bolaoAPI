4422a041f36a198c891d7676932480744086960c
luciano <lucianorocho@yahoo.com.br>
2022-11-25 23:07:06 -0300

    implementacao de resposta no chat

--- models/chat.js
+++ models/chat.js
@@ -7,7 +7,10 @@ class chat extends model{
     getChats(idGame){
         
         let sql=`select ch.*,us.name,us.photo
+            ,ch2.message as originalMessage, ch2.creation_date as originalDate, us2.name as originalUserName 
             from chat ch join user us on ch.creation_user=us.id
+            left join chat ch2 on ch.id_chat_original=ch2.id
+            left join user us2 on ch2.creation_user=us2.id
             where ch.id_game=${idGame}
             order by ch.creation_date desc `;      
         return new Promise((resolve,reject)=>{
@@ -25,9 +28,12 @@ class chat extends model{
     //ok
     getChat(idChat){
         
-        let sql=`select ch.*,us.name,us.photo
-            from chat ch join user us on ch.creation_user=us.id
-            where ch.id=${idChat}`;      
+            let sql=`select ch.*,us.name,us.photo
+                ,ch2.message as originalMessage, ch2.creation_date as originalDate, us2.name as originalUserName 
+                from chat ch join user us on ch.creation_user=us.id
+                left join chat ch2 on ch.id_chat_original=ch2.id
+                left join user us2 on ch2.creation_user=us2.id
+                where ch.id=${idChat}`;      
         return new Promise((resolve,reject)=>{
             this.executeSQL(sql)
             .then(results=>{
@@ -47,13 +53,19 @@ class chat extends model{
                 } else{
                     let valueParams=[   
                         req.fields.message,
-                        req.fields.id_game
+                        req.fields.id_game,
                     ];
 
                     let fieldParams=[   
                         'message',
-                        'id_game'
+                        'id_game',
                     ];
+
+                    if (req.fields.id_chat_original){
+                        valueParams.push(req.fields.id_chat_original);
+                        fieldParams.push('id_chat_original');
+                    }
+
                     this.save('chat',req,fieldParams,valueParams).then(results=>{
                         resolve(results);
                     }).catch(err=>{
