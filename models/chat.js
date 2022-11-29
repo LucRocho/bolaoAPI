9e3139d83b585c8d3414598c9a62e92e9e11f770
luciano <lucianorocho@yahoo.com.br>
2022-11-29 01:27:39 -0300

    nova mensagem

--- models/chat.js
+++ models/chat.js
@@ -3,21 +3,41 @@ const path = require('path');
 
 class chat extends model{
     
+
+    getNovaMensagem(searchParams){
+        let sql=`select newMessage from game_user where id_game=${searchParams['idGame']} and id_user=${searchParams['idUser']}`;      
+        return new Promise((resolve,reject)=>{
+            this.executeSQL(sql)
+            .then(results=>{
+                resolve(results[0]);
+            }).catch(er=>{
+                reject(er);
+            });
+        });         
+    }
    
     //ok
-    getChats(idGame){
-        
+    getChats(searchParams){
+
         let sql=`select ch.*,us.name,us.photo
             ,ch2.message as originalMessage, ch2.creation_date as originalDate, us2.name as originalUserName,ch2.chat_image as imageOriginal
             from chat ch join user us on ch.creation_user=us.id
             left join chat ch2 on ch.id_chat_original=ch2.id
             left join user us2 on ch2.creation_user=us2.id
-            where ch.id_game=${idGame}
+            where ch.id_game=${searchParams['idGame']}
             order by ch.creation_date desc `;      
         return new Promise((resolve,reject)=>{
             this.executeSQL(sql)
             .then(results=>{
-                resolve(results);
+
+                this.indicaMensagensLidas(searchParams['idGame'],searchParams['idUser']).then(res=>{
+                    
+                    resolve(results);
+                }).catch(er=>{
+                    reject(er);
+                });
+
+                
             }).catch(err=>{
                 reject(err);
             });
@@ -75,12 +95,47 @@ class chat extends model{
                         fieldParams.push('chat_image');
                     }
                     this.save('chat',req,fieldParams,valueParams).then(results=>{
-                        resolve(results);
+
+                        this.indicaNovaMensagem(req.fields.id_game,req.fields.loggedUserId).then(res=>{
+                            resolve(results);
+                        }).catch(er=>{
+                            reject(er);
+                        });
+
                     }).catch(err=>{
                         reject(err);
                     });
                 };
         });
+    }
+
+    indicaNovaMensagem(idGame,idUser){
+        return new Promise((resolve,reject)=>{
+            let sqlUpdate=`update game_user set newMessage=1 where id_game=${idGame} and id_user<>${idUser}`;
+            this.executeSQL(sqlUpdate).then(res=>{
+                
+                //comunicar via websockets
+
+                resolve(res)
+
+            }).catch(err=>{
+                reject(err);
+            })
+        })
+    }
+
+    indicaMensagensLidas(idGame,idUser){
+        return new Promise((resolve,reject)=>{
+            let sqlUpdate=`update game_user set newMessage=0 where id_game=${idGame} and id_user=${idUser}`;
+            this.executeSQL(sqlUpdate).then(res=>{
+
+                //comunicar via websockets
+
+                resolve(res)
+            }).catch(err=>{
+                reject(err);
+            })
+        })
     }
 }
 module.exports = chat;
--- routes/chat.route.js
+++ routes/chat.route.js
@@ -1,4 +1,5 @@
 const chat = require('../models/chat');
+const util = require('../models/util')
 
 
 module.exports=(app)=>{
@@ -18,20 +19,34 @@ module.exports=(app)=>{
 
     });
 
-    let route=app.route('/api/chats/:idGame');
-    
-    //ok
+    let route=app.route('/api/chats');
     route.get((req,res)=> {
+        
         let ch = new chat(req.app.get('env') == 'development');
-        ch.getChats(req.params.idGame).then(results=>{
+        let searchParams = util.queryStringToJSON(req.query);
+        
+        ch.getChats(searchParams).then(results=>{
             //res.setHeader('Access-Control-Allow-Origin','*');
             res.send(results);
         }).catch(err=>{
             res.send({ error: err });
         });
-
-
     });
+
+
+    let routeNewMessage=app.route('/api/chats/newmessage');
+    routeNewMessage.get((req,res)=> {
+        let ch = new chat(req.app.get('env') == 'development');
+        let searchParams = util.queryStringToJSON(req.query);
+        
+        ch.getNovaMensagem(searchParams).then(results=>{
+            //res.setHeader('Access-Control-Allow-Origin','*');
+            res.send(results);
+        }).catch(err=>{
+            res.send({ error: err });
+        });
+    });
+
 
     let routeSave=app.route('/api/chats/');
     //ok
